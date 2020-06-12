f13186c2b7e2919a86e0ab135dc440b2ad7480ad
Dolben <dolben800@gmail.com>
2020-06-12 10:15:05 +0200

    New Changes

--- lib/chat_app/talk.ex
+++ lib/chat_app/talk.ex
@@ -5,4 +5,10 @@ defmodule ChatApp.Talk do
   def list_room do
     Repo.all(Room)
   end
+
+  def create_room(attrs \\ %{}) do
+    %Room{}
+    |> Room.changeset(attrs)
+    |> Repo.insert()
+  end
 end
--- lib/chat_app_web/controllers/room_controller.ex
+++ lib/chat_app_web/controllers/room_controller.ex
@@ -1,11 +1,28 @@
 defmodule ChatAppWeb.RoomController do
   use ChatAppWeb, :controller
 
+  alias ChatApp.Talk.Room
+  alias ChatApp.Talk
+  alias ChatApp.Repo
+
   def index(conn, _params) do
     render(conn, "index.html")
   end
 
   def new(conn, _params) do
-    render(conn, "new.html")
+    changeset = Room.changeset(%Room{}, %{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"room" => room_params}) do
+    case Talk.create_room(room_params) do
+      {:ok, room} ->
+        conn
+        |> put_flash(:info, "Room Created!")
+        |> redirect(to: Routes.room_path(conn, :index))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
   end
 end
